8dede1ffac2a7fdacba81ddcf954076b737b9fd1
Grace Nguimatsa <nguimatsagrace123@gmail.com>
2025-10-18 16:20:02 -0700

    Create task_5.sql to insert a customer record

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- task_5.sql
+
+-- Select the alx_book_store database explicitly
+USE alx_book_store;
+
+-- Insert a single row into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
