56adef2094ca25fca1acf82d802cfb3a498710ae
Grace Nguimatsa <nguimatsagrace123@gmail.com>
2025-10-18 15:51:59 -0700

    Create task_4.sql to show book table columns

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- task_4.sql
+
+-- Select the alx_book_store database explicitly
+USE alx_book_store;
+
+-- Prints the full description of the 'books' table,
+-- which is the alternative to using DESCRIBE or EXPLAIN.
+SHOW FULL COLUMNS FROM books;
